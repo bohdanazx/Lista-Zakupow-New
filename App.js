1f8ed64fe4db60d4bc4693ee644e01325ebfcd12
bohdanayabl <little.elf2004@gmail.com>
2025-04-17 14:20:32 +0200

    navigation2

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-// App.js
 import React from "react";
-// Це головний контейнер, який відповідає за всю навігацію в додатку. Це як рамка або обгортка, яка дозволяє переходити між екранами в нашому мобільному додатку.
 import { NavigationContainer } from "@react-navigation/native";
-// Це тип навігації, де екрани накладаються один на інший, як карти (stack = стек).
-// Уявіть, що кожен екран — це сторінка. Ми «штовхаємо» нову сторінку зверху і можемо «повернутись назад». Це зручно для мобільних додатків.
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "./screens/LoginScreen";
@@ -11,7 +7,6 @@ import HomeScreen from "./screens/HomeScreen";
 import AddProductScreen from "./screens/AddProductScreen";
 import ProductDetailScreen from "./screens/ProductDetailScreen";
 
-// Сам навігатор. Stack — це змінна, в якій зберігається об'єкт для керування переходами між екранами. Створити навігаційну структуру, в якій кожен екран додається у вигляді стеку.
 const Stack = createNativeStackNavigator();
 
 export default function App() {
